5cafe7b52b957edf5a8840e0d45174518ba47753
LMHoe <pcmker4@naver.com>
2018-04-21 22:02:25 +0900

    hw1_6

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -5,60 +5,56 @@ package edu.handong.csee.java.lab09;
  *  : get Data from user, calculate average & highest sales, print out data*/
 
 import java.util.Scanner;//to get input
+import java.util.ArrayList;//import Array list utility
 
 public class SalesReporter {
 	
 	private double highestSales=0;//set highestSales as 0
 	private double averageSales=0;//set averageSales as 0
-	
-	private SalesAssociate[] team;//make array of associate name & sales
-	
-	private int numberOfAssociates;//get number of associate
+	ArrayList<String> name = new ArrayList<String>();//arraylist of name
+	ArrayList<Double> sales = new ArrayList<Double>();//arraylist of sales
+	boolean moredata=true;//boolean data
 	
 	Scanner keyboard = new Scanner(System.in);// to get user input
 	
 	
 	void getData() { //get data from user
 		
-		System.out.print("Enter number of sales associates : ");
-		this.numberOfAssociates = keyboard.nextInt();//get number of associate
+		do {
+			System.out.print("Enter name of sales associate: ");//ask name
+			this.name.add(keyboard.nextLine());//get associate name
 		
-		team = new SalesAssociate[numberOfAssociates];//set array team's memory
+			System.out.print("Enter associate's sales : $");//ask sales
+			this.sales.add(keyboard.nextDouble());//get associate Sales
 		
-		for(int i=0; i<team.length;i++) {
+			System.out.print("another data(yes or no) : ");//ask more data
+			String S = keyboard.nextLine();//get \n
+			S = keyboard.nextLine();//get answer
 			
-			System.out.println("Enter data for associate number "+(i+1));
-			System.out.print("Enter name of sales associate: ");
-			String s = keyboard.nextLine();//get \n
-			s = keyboard.nextLine();//get associate name
-
+			if (S.equals("yes")) {
+				this.moredata = true;
+			}
+			else {
+				this.moredata = false;
+			}
 			
-			System.out.print("Enter associate's sales : $");
-			int l= keyboard.nextInt();//get associate Sales
-			
-			SalesAssociate SalesMan=new SalesAssociate();//temp SalesAssociate variable SalesMan
-			SalesMan.setName(s);//set Name of SalesMan
-			SalesMan.setSales(l);//set Sales of SalesMan
-			
-			team[i]=SalesMan;//put SalesMan data into array
-			
-		}//do process for team.length times
+		}while(moredata);//do process for while answer is no
 		
 	}
 	
 	void comuteStats() {//find out highest Sales & average Sales
 		
-		for(int i=0; i<numberOfAssociates;i++) {
+		for(int i=1; i<=this.name.size();i++) {
 			
-			if(team[i].getSales()>highestSales) {
-				this.highestSales = team[i].getSales();//change highestSales value
+			if(this.sales.get(i)>highestSales) {
+				this.highestSales = this.sales.get(i);//change highestSales value
 			}//if member i's sales is higher than highestSales than change highestSales as i's sales value
 			
-			this.averageSales = averageSales+team[i].getSales();//add every members' sales
+			this.averageSales = averageSales+this.sales.get(i);//add every members' sales
 			
 		}//do process for number of associate times
 		
-		this.averageSales = averageSales/numberOfAssociates;//calculate averageSales
+		this.averageSales = averageSales/this.name.size();//calculate averageSales
 		
 	}
 	
@@ -68,35 +64,35 @@ public class SalesReporter {
 		System.out.println("The highest sales figure is $"+highestSales);//print out highest sales
 		System.out.println("\nThe following had the highest sales:");//print out data of highest sales member
 		
-		for(int i=0; i<numberOfAssociates;i++) {
+		for(int i=1; i<=name.size();i++) {
 			
-			if(team[i].getSales()==highestSales) {
+			if(this.sales.get(i)==highestSales) {
 				
-				System.out.println("Name: "+team[i].getName());//print out member's name
-				System.out.println("Sales: $"+team[i].getSales());//print out member's sales
-				System.out.println("$"+(team[i].getSales()-averageSales)+" above the average.");//print out difference between average Sales & member's sales	
+				System.out.println("Name: "+this.name.get(i));//print out member's name
+				System.out.println("Sales: $"+this.sales.get(i));//print out member's sales
+				System.out.println("$"+(this.sales.get(i)-averageSales)+" above the average.");//print out difference between average Sales & member's sales	
 			}//print out if member's Sale equals to highestSales
 		}//do process for number of associate times
 		
 		System.out.println("\nThe rest performed as follows:");//print out rest members' data
 		
-		for(int i=0; i<numberOfAssociates;i++) {
+		for(int i=0; i<this.name.size();i++) {
 			
-			if(team[i].getSales()!=highestSales) {
+			if(this.sales.get(i)!=highestSales) {
 				
-				if(team[i].getSales()<averageSales) {
+				if(this.sales.get(i)<averageSales) {
 					
-					System.out.println("Name: "+team[i].getName());//print out member's name
-					System.out.println("Sales: $"+team[i].getSales());//print out member's sales
-					System.out.println("$"+(averageSales-team[i].getSales())+" below the average.");//print out difference between average sale & member's sales
+					System.out.println("Name: "+this.name.get(i));//print out member's name
+					System.out.println("Sales: $"+this.sales.get(i));//print out member's sales
+					System.out.println("$"+(averageSales-this.sales.get(i))+" below the average.");//print out difference between average sale & member's sales
 					
 				}//print out if member's sales is lower than average sales
 				
 				else {
 					
-					System.out.println("Name: "+team[i].getName());//print out member's name
-					System.out.println("Sales: $"+team[i].getSales());//print out member's sales
-					System.out.println("$"+(team[i].getSales()-averageSales)+" above the average.");//print out difference between average sales & member's sales
+					System.out.println("Name: "+this.name.get(i));//print out member's name
+					System.out.println("Sales: $"+this.sales.get(i));//print out member's sales
+					System.out.println("$"+(this.sales.get(i)-averageSales)+" above the average.");//print out difference between average sales & member's sales
 					
 				}////print out if member's sales is lower than average sales
 			}//do this process if member's sales is not equals to highestSales 
